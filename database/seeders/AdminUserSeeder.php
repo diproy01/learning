aebc7c5bec58cc3f9c70d391391b43f23f681392
Roy Dipak <dipak.cse.diu@gmail.com>
2026-06-02 14:55:48 +0900

    Add default admin user seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make('Admin@123'),
+            ]
+        );
+
+        $user->syncRoles(['admin']);
+    }
+}
